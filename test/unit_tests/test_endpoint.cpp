c09e252109f1815b2304b4bf3d20bcfc6be4c420
David Keller <david.keller@litchis.fr>
2015-07-19 21:42:41 +0200

    Add endpoint unit tests.

---
+++ test/unit_tests/test_endpoint.cpp
@@ -0,0 +1,69 @@
+// Copyright (c) 2013-2014, David Keller
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the University of California, Berkeley nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY DAVID KELLER AND CONTRIBUTORS ``AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "helpers/common.hpp"
+#include <kademlia/endpoint.hpp>
+
+namespace k = kademlia;
+
+BOOST_AUTO_TEST_SUITE( test_construction )
+
+BOOST_AUTO_TEST_CASE( can_be_constructed_with_service_as_string )
+{
+    k::endpoint{ "127.0.0.1", "1234" };
+}
+
+BOOST_AUTO_TEST_CASE( can_be_constructed_with_service_as_integer )
+{
+    k::endpoint{ "127.0.0.1", 1234 };
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE( test_getter_and_setters )
+
+BOOST_AUTO_TEST_CASE( can_be_inspected_and_modified )
+{
+    k::endpoint e{ "127.0.0.1", "1234" };
+    BOOST_REQUIRE_EQUAL( "127.0.0.1", e.address() );
+    BOOST_REQUIRE_EQUAL( "1234", e.service() );
+
+    e.address( "192.168.0.1" );
+    e.service( "4567" );
+    BOOST_REQUIRE_EQUAL( "192.168.0.1", e.address() );
+    BOOST_REQUIRE_EQUAL( "4567", e.service() );
+}
+
+BOOST_AUTO_TEST_CASE( can_be_printed )
+{
+    boost::test_tools::output_test_stream out;
+
+    out << k::endpoint{ "127.0.0.1", 1234 };
+
+    BOOST_CHECK( out.is_equal( "127.0.0.1:1234" ) );
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
